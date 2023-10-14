a1b71d5695fda33c1e9cb504ea614959b57d8700
pechencka26 <114876085+pechencka26@users.noreply.github.com>
2023-10-15 01:29:58 +0300

    Лабараторная работа 1

---
+++ Лабараторная работа 1/task3.py
@@ -0,0 +1,8 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+count=len(list_players)
+team1=list_players[3:6]
+team2=list_players[0:3]
+
+print(team2)
+print(team1)
+# TODO Разделите участников на две команды
